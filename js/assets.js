1cd3241d5226cff7515abe1f69413ffa270ec9e9
Kevin Owens <kowens@gmail.com>
2015-03-04 16:38:41 -0700

    Added new folk song, 'Woah, Haw, Buck and Jerry Boy'

--- js/assets.js
+++ js/assets.js
@@ -26,6 +26,7 @@ var spriteAssets = {
 
 var audioAssets = [
 	{ 'src': 'audio/let-the-mountains-shout-for-joy.mp3' },
+	{ 'src': 'audio/woah-haw-buck-and-jerry-boy.mp3' },
 	{ 'src': 'audio/come-come-ye-saints.mp3' },
 	{ 'src': 'audio/pioneer-children-sang-as-they-walked.mp3' },
 	{ 'src': 'audio/the-handcart-song.mp3' },
